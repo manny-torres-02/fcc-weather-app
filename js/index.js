8c93b2b2e2e7bd383dff7c13f726b079840d9438
Manuel Torres <manueljosetorres02@gmail.com>
2017-08-10 23:54:13 -0500

    Remove CORS Request
    use Heroku Request https://cors-anywhere.herokuapp.com/

--- js/index.js
+++ js/index.js
@@ -3,19 +3,19 @@ var API_KEY = "4d4fa64149028409712cabd7de63ab95"
 
 $(function() {
   //Geolocation api map key - USE IPinfo- THANKS!
-  var loc 
-  
+  var loc
+
   $.getJSON('https://ipinfo.io', function(d){
   console.log("assign the data")
      loc= d.loc.split(",")
     console.log(loc);
-    
+
    var lat=loc[0]
    var lon=loc[1]
    //console.log(lat)
    //console.log(lon)
-   
-$.getJSON("https://crossorigin.me/https://api.darksky.net/forecast/"+API_KEY+"/"+lat+","+lon+"/?exclude=minutely,daily?", function(wd) {
+
+$.getJSON("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/"+API_KEY+"/"+lat+","+lon+"/?exclude=minutely,daily?", function(wd) {
       console.log( "get data", wd);
       var currentLocation=wd.timezone;
       //console.log(currentLocation);
@@ -23,19 +23,19 @@ $.getJSON("https://crossorigin.me/https://api.darksky.net/forecast/"+API_KEY+"/"
       //console.log(currentWeather)
     var currentTemp=wd.currently.temperature
     //console.log(currentTemp);
-   
+
     $(".location").html(currentLocation)
     $(".currentWeather").html(currentWeather)
   $(".currentTemp").html(currentTemp)
 
-    
-       
+
+
 		//Skycons
-		
+
   var iconRequest = wd.currently.icon;
-			
+
 			var icons = new Skycons({'color' : '#f26304'});
-			
+
 			var iconList = [
 				"clear-day",
 				"clear-night",
@@ -47,35 +47,35 @@ $.getJSON("https://crossorigin.me/https://api.darksky.net/forecast/"+API_KEY+"/"
 				"snow",
 				"wind",
 				"fog"
-			];		
+			];
 			console.log(icons);
 			for (i = 0; i < iconList.length; i++) {
 				if (iconRequest == iconList[i]) {
 						icons.set('icon', iconList[i]);
-					
+
 				}
 			}
 			icons.play();
-  
-  //Convert Fahrenheit to Celsius 
+
+  //Convert Fahrenheit to Celsius
   var celsius= (currentTemp-32)*(5/9)
   var roundC= Math.round(celsius);
   var roundF=Math.round(currentTemp)
   console.log(currentTemp)
   console.log(roundC)
-  //Toggle fahrenheit and celsius - conditionals 
+  //Toggle fahrenheit and celsius - conditionals
   $("#toggle").on("click", function(){
-    if ($(this).text() ==="Fahrenheit") 
+    if ($(this).text() ==="Fahrenheit")
       {
         $(".temp").html(roundC+"C")
         $(this).text("Celsius")
       }
-    else 
+    else
       {$(".temp").html(roundF+"F");
       $(this).text("Fahrenheit");
       }
   })
-}) 
+})
 });
 })
  //IP info Api Access Token   05f14825a2763c
